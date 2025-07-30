1d8bb890250205a8380af2e2cd0a7bbb416883c2
Enak Junior <mamadoukanejunior@gmail.com>
2025-07-30 20:23:14 +0000

    Mise en place de l'architecture du projet.

---
+++ src/app/feature/Ressources/components/ressource-detail/ressource-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ressource-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './ressource-detail.component.html',
+  styleUrl: './ressource-detail.component.css'
+})
+export class RessourceDetailComponent {
+
+}
