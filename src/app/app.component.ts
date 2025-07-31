b1b602279e1f7a63d0295256071ddef7c298e54f
Enak Junior <mamadoukanejunior@gmail.com>
2025-07-31 18:56:21 +0000

    Finission des copies from old project et réajustement du code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MainPageComponent} from "./shared/pages/main-page/main-page.component";
+import {HeaderComponent} from "./shared/pages/header/header.component";
+import {FooterComponent} from "./shared/pages/footer/footer.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [MainPageComponent, HeaderComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
---
+++ src/app/shared/pages/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-main-page',
+  imports: [RouterOutlet],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+}
